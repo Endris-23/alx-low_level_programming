2722f76b4e0c2d6255e3bb1e2316c7b42452c5bd
Endris-23 <edrisnuri9@gmail.com>
2023-08-04 18:24:20 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,54 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-/**
- * check_num - check string there are digit
- * @str: array str
- * Return: Always 0 (success)
- */
-int check_num(char *str)
-{
-	unsigned int count;
-
-	count = 0;
-	while (count < strlen(str))
-	{
-		if (!isdigit(str[count]))
-		{
-			return (0);
-		}  
-		count++;
-	}
-	return (1);
-}
 /**
  * main - print the name of the program
- * @argc: count arguments
- * @argv: arguments
+ * @argc: the number of  arguments
+ * @argv: array of  arguments
  * Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	int count;
-	int str_to_int;
 	int sum = 0;
-	
-	count = 1;
-	while (count < argc)
+	char *c;
+
+	while (--argc)
 	{
-		if(check_num(argv[count]))
-		{
-			str_to_int = atoi(argv[count]);
-			sum += str_to_int;
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-		count++;
-	}
 	printf("%d\n", sum);
 	return (0);
 }
