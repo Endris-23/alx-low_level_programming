02114730c245d6b584221da497d9c2fd6ad9afe8
Endris-23 <edrisnuri9@gmail.com>
2023-10-07 03:12:01 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the program
+ * @argc: the number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
